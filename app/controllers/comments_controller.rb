da88d49ea3ed6f8ceddefe369e00fc0d4c694673
Raven <ravenkoehler@gmail.com>
2021-10-13 10:49:49 -0400

    added edit action for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,11 @@ class CommentsController < ApplicationController
         comment.save
         render json: comment
     end
+
+    def edit
+        comment = Comment.find(params["id"])
+        comment.text = "#{params["user"]["name"]} says: #{params["text"]}"
+        comment.save
+        render json: comment
+    end
 end
